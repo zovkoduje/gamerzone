f9746bd1c2a1607e8e52f61fd42fee2582dc03f7
Duje Zovko <dujezovko@hotmail.com>
2024-09-14 10:43:12 +0200

    brisanje produkta

--- pages/products/delete/[...id].js
+++ pages/products/delete/[...id].js
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 import axios from "axios";
 import { useState } from "react";
+import Swal from 'sweetalert2'; // Added SweetAlert import
 
 export default function DeleteProductPage(){
     const router=useRouter();
@@ -26,19 +27,29 @@ export default function DeleteProductPage(){
 
     return(
         <Layout>
-            <h1 className="text-center">Do you really want to delete &nbsp;{productInfo?.title}?</h1>
-            <div className="flex gap-2 justify-center">
-                <button className='btn-primary flex' onClick={deleteProduct}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
-                </svg>
-                Yes</button>
-                <button className='btn-primary flex' onClick={goBack}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-                </svg>
-                No</button>
+            <div className="mt-20">
+                <h1 className="text-center">Do you really want to delete &nbsp;{productInfo?.title}?</h1>
+                <div className="flex gap-2 justify-center">
+                    <button className='btn-primary flex' onClick={async () => { 
+                        await deleteProduct();
+                        Swal.fire({
+                            title: 'Success',
+                            text: 'Product deleted successfully!',
+                            icon: 'success',
+                            confirmButtonColor: 'black',
+                        });
+                    }}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                    </svg>
+                    Yes</button>
+                    <button className='btn-primary flex' onClick={goBack}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                    </svg>
+                    No</button>
+                </div>
             </div>
-            </Layout>
+        </Layout>
     )
 }
